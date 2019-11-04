9214c6bcb37571808666d2f46f8039ff128f2ab5
Marc Buma <mosbuma@bumos.nl>
2019-11-04 20:35:16 +0100

    fix blockchain

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,49 +1,31 @@
 //index.js
 const { Application, genesisBlockDevnet, configDevnet } = require('lisk-sdk'); // require application class, the default genesis block and the default config for the application
-const CreateBikeTransaction = require('../import/api/lisk-blockchain/transactions/create-bike'); // require the newly created transaction type 'HelloTransaction'
-const FaucetTransaction = require('../import/api/lisk-blockchain/transactions/faucet'); // require the newly created transaction type 'HelloTransaction'
-const UpdateBikeLocationTransaction = require('../import/api/lisk-blockchain/transactions/update-bike-location'); // require the newly created transaction type 'HelloTransaction'
-const RentBikeTransaction = require('../import/api/lisk-blockchain/transactions/rent-bike'); // require the newly created transaction type 'HelloTransaction'
-const ReturnBikeTransaction = require('../import/api/lisk-blockchain/transactions/return-bike');
+const CreateBikeTransaction = require('../app/imports/api/lisk-blockchain/transactions/create-bike'); // require the newly created transaction type 'HelloTransaction'
+const FaucetTransaction = require('../app/imports/api/lisk-blockchain/transactions/faucet'); // require the newly created transaction type 'HelloTransaction'
+const UpdateBikeLocationTransaction = require('../app/imports/api/lisk-blockchain/transactions/update-bike-location'); // require the newly created transaction type 'HelloTransaction'
+const RentBikeTransaction = require('../app/imports/api/lisk-blockchain/transactions/rent-bike'); // require the newly created transaction type 'HelloTransaction'
+const ReturnBikeTransaction = require('../app/imports/api/lisk-blockchain/transactions/return-bike');
 
 configDevnet.app.label = 'lisk-bike-blockchain-app'; // change the label of the app
 configDevnet.components.storage.password = 'lisk123'; // replace password with the password for your database user
-configDevnet.components.logger.logFileName = '/storage/lisk.bike/lisk-logs/devnet/lisk.log'; // define name and path of the log file. Default: logs/lisk.log
-configDevnet.components.fileLogLevel = "debug"; // Minimum loglevel, that should be logged in the log file. Available values: trace, debug, info(default), warn, error, fatal, none.
-configDevnet.components.consoleLogLevel = "node";// Minimum loglevel, that should be logged in the console, when starting the node. Available values: trace, debug, info, warn, error, fatal, none(default).
 
 configDevnet.modules.http_api.access.public = true;
 
-// create the log folder
-const path = require("path");
-const fs = require("fs");
+// Create the application instance
+const app = new Application(genesisBlockDevnet, configDevnet);
 
-const launchBlockchain=false;
+// Register custom transactions
+app.registerTransaction(CreateBikeTransaction);
+app.registerTransaction(FaucetTransaction);
+app.registerTransaction(UpdateBikeLocationTransaction);
+app.registerTransaction(RentBikeTransaction);
+app.registerTransaction(ReturnBikeTransaction);
 
-// use commandline parameter to lauch blockchain, prevent meteor from automatically starting this process
-if(launchBlockchain==true) {
-  let logdir = path.dirName(configDevnet.components.logger.logFileName);
-  if (!fs.existsSync(logdir)){
-    console.log("create lisk logdir %s", logdir);
-    fs.mkdirSync(logdir);
-  }
-
-  // Create the application instance
-  const app = new Application(genesisBlockDevnet, configDevnet);
-
-  // Register custom transactions
-  app.registerTransaction(CreateBikeTransaction);
-  app.registerTransaction(FaucetTransaction);
-  app.registerTransaction(UpdateBikeLocationTransaction);
-  app.registerTransaction(RentBikeTransaction);
-  app.registerTransaction(ReturnBikeTransaction);
-
-  // the code block below starts the application and doesn't need to be changed
-  app
-      .run()
-      .then(() => app.logger.info('App started...'))
-      .catch(error => {
-          console.error('Faced error in application', error);
-          process.exit(1);
-      });
-}
+// the code block below starts the application and doesn't need to be changed
+app
+    .run()
+    .then(() => app.logger.info('App started...'))
+    .catch(error => {
+        console.error('Faced error in application', error);
+        process.exit(1);
+    });
